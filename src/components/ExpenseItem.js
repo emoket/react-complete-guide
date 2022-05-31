4d138ec23c12adc600091050bd5f1430b51aaf60
박용진 <insight.emoket@gmail.com>
2022-05-31 15:46:12 +0900

    react props

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <p className='expense-item__price'>${props.amount}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
