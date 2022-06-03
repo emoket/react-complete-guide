bb627f43e1a1f12516b07885c8238b7a15ae273a
박용진 <insight.emoket@gmail.com>
2022-06-03 15:28:09 +0900

    CSS Module test

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,12 +1,12 @@
 import ChartBar from './ChartBar';
-import './Chart.css';
+import styles from './Chart.module.css';
 
 const Chart = (props) => {
   let chartValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   const maximumValue = Math.max(...chartValues);
 
   return (
-    <div className='chart'>
+    <div className={styles.chart}>
       {props.dataPoints.map((dataPoint) => {
         return (
           <ChartBar
