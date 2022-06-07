c07bb82266c84b84f55cfdd6427ca20290f5e321
박용진 <insight.emoket@gmail.com>
2022-06-07 14:56:56 +0900

    async await로 변경

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,6 +1,6 @@
 import { API_BASE_URL } from '../api-config';
 
-const call = (api, method, request) => {
+const call = async (api, method, request) => {
   let options = {
     headers: new Headers({
       'Content-Type': 'application/json',
@@ -14,12 +14,13 @@ const call = (api, method, request) => {
     options.body = JSON.stringify(request);
   }
 
-  return fetch(options.url, options).then((response) => {
-    response.json().then((json) => {
-      // 정상 응답이 아니면 reject Promise
-      if (!response.ok) Promise.reject(json);
-      return json;
-    });
+  const response = await fetch(options.url, options);
+  response.json().then((json) => {
+    // 정상 응답이 아니면 reject Promise
+    if (!response.ok) {
+      Promise.reject(json);
+    }
+    return json;
   });
 };
 
