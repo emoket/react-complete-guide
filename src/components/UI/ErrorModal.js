e2221426686e958825571969e33c01a26cdf647d
박용진 <insight.emoket@gmail.com>
2022-06-17 10:40:11 +0900

    using ReactDOM.createPortal to render target HTML

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,45 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
-const ErrorModal = (props) => {
+const Backdrop = ({ onHideModal }) => {
+  return <div className={classes.backdrop} onClick={onHideModal} />;
+};
+
+const ModalOverlay = ({ title, message, onHideModal }) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onHideModal} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onHideModal}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={onHideModal}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = ({ title, message, onHideModal }) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onHideModal={onHideModal} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          message={message}
+          onHideModal={onHideModal}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 };
 
